2f1792cbf6e5ab8bc4702b6e4270572df4c51b64
Brett Rehberg <brett.rehberg@gmail.com>
2023-12-06 05:35:35 +0000

    Day 6: Wait For It

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,31 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(input) do
+    String.split(input, "\n", trim: true)
+    |> Enum.map(fn line ->
+      Regex.scan(~r/(\d+)/, line, capture: :all_but_first)
+      |> Enum.map(fn s -> String.to_integer(hd(s)) end)
+    end)
+    |> List.zip()
+    |> Enum.map(&compute_ways(&1, elem(&1, 0) - 1, 0))
+    |> Enum.product()
   end
 
-  def part2(_args) do
+  defp compute_ways({_, _}, 0, ways), do: ways
+
+  defp compute_ways({time, best}, hold, ways) do
+    if(hold * (time - hold) > best,
+      do: compute_ways({time, best}, hold - 1, ways + 1),
+      else: compute_ways({time, best}, hold - 1, ways)
+    )
+  end
+
+  def part2(input) do
+    [time, best] =
+      String.split(input, "\n", trim: true)
+      |> Enum.map(fn line ->
+        Regex.scan(~r/(\d+)/, line, capture: :all_but_first) |> Enum.join() |> String.to_integer()
+      end)
+
+    compute_ways({time, best}, time - 1, 0)
   end
 end
--- test/advent_of_code/day_06_test.exs
+++ test/advent_of_code/day_06_test.exs
@@ -3,19 +3,37 @@ defmodule AdventOfCode.Day06Test do
 
   import AdventOfCode.Day06
 
-  @tag :skip
+  # @tag :skip
   test "part1" do
-    input = nil
+    input = "Time:      7  15   30
+Distance:  9  40  200"
     result = part1(input)
 
-    assert result
+    assert result == 288
   end
 
-  @tag :skip
+  # @tag :skip
   test "part2" do
-    input = nil
+    input = "Time:      7  15   30
+Distance:  9  40  200"
     result = part2(input)
 
-    assert result
+    assert result == 71503
+  end
+
+  # @tag :skip
+  test "full_part1" do
+    input = AdventOfCode.Input.get!(6, 2023)
+    result = part1(input)
+
+    assert result == 393_120
+  end
+
+  # @tag :skip
+  test "full_part2" do
+    input = AdventOfCode.Input.get!(6, 2023)
+    result = part2(input)
+
+    assert result == 36_872_656
   end
 end

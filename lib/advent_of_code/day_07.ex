7e3028831528a7cce737cd1504c500c9ed1dfb6a
Brett Rehberg <brett.rehberg@gmail.com>
2023-12-07 06:30:26 +0000

    Day 7: Camel Cards

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,82 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+  def part1(input) do
+    String.split(input, "\n", trim: true)
+    |> Enum.map(&parse_hand/1)
+    |> Enum.sort_by(&score_hand/1)
+    |> total_winnings(1, 0)
   end
 
-  def part2(_args) do
+  defp parse_hand(line) do
+    [hand, bid] = String.split(line)
+    {String.split(hand, "", trim: true), String.to_integer(bid)}
+  end
+
+  defp total_winnings([], _, sum), do: sum
+
+  defp total_winnings([{_, bid} | rest], n, sum),
+    do: total_winnings(rest, n + 1, sum + bid * n)
+
+  defp score_hand({cards = [a, b, c, d, e], _}) do
+    get_type(Enum.sort(cards)) * 10_000_000_000 +
+      card_value(a) * 100_000_000 +
+      card_value(b) * 1_000_000 +
+      card_value(c) * 10000 +
+      card_value(d) * 100 +
+      card_value(e) * 1
+  end
+
+  defp card_value("A"), do: 14
+  defp card_value("K"), do: 13
+  defp card_value("Q"), do: 12
+  defp card_value("J"), do: 11
+  defp card_value("T"), do: 10
+  defp card_value("X"), do: 1
+  defp card_value(d), do: String.to_integer(d)
+
+  # Five of a kind
+  defp get_type([c, c, c, c, c]), do: 6
+  defp get_type([c, c, c, c, x]) when x == "X", do: 6
+  defp get_type([c, c, c, x, x]) when x == "X", do: 6
+  defp get_type([c, c, x, x, x]) when x == "X", do: 6
+  defp get_type([_, x, x, x, x]) when x == "X", do: 6
+  # Four of a kind
+  defp get_type([c, c, c, c, _]), do: 5
+  defp get_type([_, c, c, c, c]), do: 5
+  defp get_type([c, c, c, _, x]) when x == "X", do: 5
+  defp get_type([_, c, c, c, x]) when x == "X", do: 5
+  defp get_type([c, c, _, x, x]) when x == "X", do: 5
+  defp get_type([_, c, c, x, x]) when x == "X", do: 5
+  defp get_type([_, _, x, x, x]) when x == "X", do: 5
+  # Full house
+  defp get_type([c, c, c, y, y]), do: 4
+  defp get_type([y, y, c, c, c]), do: 4
+  defp get_type([c, c, y, y, x]) when x == "X", do: 4
+  # Three of a kind
+  defp get_type([c, c, c, _, _]), do: 3
+  defp get_type([_, c, c, c, _]), do: 3
+  defp get_type([_, _, c, c, c]), do: 3
+  defp get_type([c, c, _, _, x]) when x == "X", do: 3
+  defp get_type([_, c, c, _, x]) when x == "X", do: 3
+  defp get_type([_, _, c, c, x]) when x == "X", do: 3
+  defp get_type([_, _, _, x, x]) when x == "X", do: 3
+  # Two pair
+  defp get_type([c, c, y, y, _]), do: 2
+  defp get_type([c, c, _, y, y]), do: 2
+  defp get_type([_, c, c, y, y]), do: 2
+  # One pair
+  defp get_type([c, c, _, _, _]), do: 1
+  defp get_type([_, c, c, _, _]), do: 1
+  defp get_type([_, _, c, c, _]), do: 1
+  defp get_type([_, _, _, c, c]), do: 1
+  defp get_type([_, _, _, _, x]) when x == "X", do: 1
+  # High card
+  defp get_type(_), do: 0
+
+  def part2(input) do
+    String.replace(input, "J", "X")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_hand/1)
+    |> Enum.sort_by(&score_hand/1)
+    |> total_winnings(1, 0)
   end
 end
--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -3,19 +3,41 @@ defmodule AdventOfCode.Day07Test do
 
   import AdventOfCode.Day07
 
-  @tag :skip
+  @example "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"
+
+  # @tag :skip
   test "part1" do
-    input = nil
+    input = @example
     result = part1(input)
 
-    assert result
+    assert result == 6440
   end
 
-  @tag :skip
+  # @tag :skip
   test "part2" do
-    input = nil
+    input = @example
     result = part2(input)
 
-    assert result
+    assert result == 5905
+  end
+
+  # @tag :skip
+  test "full_part1" do
+    input = AdventOfCode.Input.get!(7, 2023)
+    result = part1(input)
+
+    assert result == 251_545_216
+  end
+
+  # @tag :skip
+  test "full_part2" do
+    input = AdventOfCode.Input.get!(7, 2023)
+    result = part2(input)
+
+    assert result == 250_384_185
   end
 end

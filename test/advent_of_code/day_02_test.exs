60aa14ce38c897dda721738ce52e4e806f126330
Brett Rehberg <brett.rehberg@gmail.com>
2023-12-07 06:37:14 +0000

    Day 2: Cube Conundrum

--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,13 +3,15 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
-  # @tag :skip
-  test "part1" do
-    input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+  @example "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+
+  # @tag :skip
+  test "part1" do
+    input = @example
     result = part1(input)
 
     assert result == 8
@@ -17,11 +19,7 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
 
   # @tag :skip
   test "part2" do
-    input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    input = @example
     result = part2(input)
 
     assert result == 2286

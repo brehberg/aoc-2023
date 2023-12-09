75c76c9f8aa0dbfa16de0c804bff74041f19f3d9
Brett Rehberg <brett.rehberg@gmail.com>
2023-12-09 12:55:32 +0000

    Day 9: Mirage Maintenance

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,36 +1,40 @@
 defmodule AdventOfCode.Day09 do
+  import Enum
+
   def part1(input) do
     String.split(input, "\n", trim: true)
-    |> Enum.map(fn line -> String.split(line) |> Enum.map(&String.to_integer/1) end)
-    |> Enum.map(&find_sequence_next/1)
-    |> Enum.sum()
+    |> map(&parse_line/1)
+    |> map(&find_sequence_next/1)
+    |> sum()
   end
 
   defp find_sequence_next(values) do
-    seq =
-      Enum.chunk_every(values, 2, 1, :discard)
-      |> Enum.map(fn [a, b] -> b - a end)
+    seq = calc_differences(values)
 
     cond do
-      Enum.all?(seq, &(&1 == 0)) -> List.last(values)
+      all?(seq, &(&1 == 0)) -> List.last(values)
       true -> find_sequence_next(seq) + List.last(values)
     end
   end
 
+  defp calc_differences(values),
+    do: chunk_every(values, 2, 1, :discard) |> map(fn [a, b] -> b - a end)
+
+  defp parse_line(line),
+    do: String.split(line) |> map(&String.to_integer/1)
+
   def part2(input) do
     String.split(input, "\n", trim: true)
-    |> Enum.map(fn line -> String.split(line) |> Enum.map(&String.to_integer/1) end)
-    |> Enum.map(&find_sequence_prev/1)
-    |> Enum.sum()
+    |> map(&parse_line/1)
+    |> map(&find_sequence_prev/1)
+    |> sum()
   end
 
   defp find_sequence_prev(values) do
-    seq =
-      Enum.chunk_every(values, 2, 1, :discard)
-      |> Enum.map(fn [a, b] -> b - a end)
+    seq = calc_differences(values)
 
     cond do
-      Enum.all?(seq, &(&1 == 0)) -> hd(values)
+      all?(seq, &(&1 == 0)) -> hd(values)
       true -> hd(values) - find_sequence_prev(seq)
     end
   end

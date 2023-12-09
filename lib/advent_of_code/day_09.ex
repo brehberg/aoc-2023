2ed89bce8eb18ebc636c2d0098e6638cba2c0a57
Brett Rehberg <brett.rehberg@gmail.com>
2023-12-09 06:39:26 +0000

    Day 9: Mirage Maintenance

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,7 +1,37 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  def part1(input) do
+    String.split(input, "\n", trim: true)
+    |> Enum.map(fn line -> String.split(line) |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(&find_sequence_next/1)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  defp find_sequence_next(values) do
+    seq =
+      Enum.chunk_every(values, 2, 1, :discard)
+      |> Enum.map(fn [a, b] -> b - a end)
+
+    cond do
+      Enum.all?(seq, &(&1 == 0)) -> List.last(values)
+      true -> find_sequence_next(seq) + List.last(values)
+    end
+  end
+
+  def part2(input) do
+    String.split(input, "\n", trim: true)
+    |> Enum.map(fn line -> String.split(line) |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(&find_sequence_prev/1)
+    |> Enum.sum()
+  end
+
+  defp find_sequence_prev(values) do
+    seq =
+      Enum.chunk_every(values, 2, 1, :discard)
+      |> Enum.map(fn [a, b] -> b - a end)
+
+    cond do
+      Enum.all?(seq, &(&1 == 0)) -> hd(values)
+      true -> hd(values) - find_sequence_prev(seq)
+    end
   end
 end
--- test/advent_of_code/day_09_test.exs
+++ test/advent_of_code/day_09_test.exs
@@ -3,19 +3,39 @@ defmodule AdventOfCode.Day09Test do
 
   import AdventOfCode.Day09
 
-  @tag :skip
+  @example "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"
+
+  # @tag :skip
   test "part1" do
-    input = nil
+    input = @example
     result = part1(input)
 
-    assert result
+    assert result == 114
   end
 
-  @tag :skip
+  # @tag :skip
   test "part2" do
-    input = nil
+    input = @example
     result = part2(input)
 
-    assert result
+    assert result == 2
+  end
+
+  # @tag :skip
+  test "full_part1" do
+    input = AdventOfCode.Input.get!(9, 2023)
+    result = part1(input)
+
+    assert result == 1_861_775_706
+  end
+
+  # @tag :skip
+  test "full_part2" do
+    input = AdventOfCode.Input.get!(9, 2023)
+    result = part2(input)
+
+    assert result == 1082
   end
 end
